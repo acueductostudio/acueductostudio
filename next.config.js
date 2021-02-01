6d653c61fee40451d51dcad03d10c42c30087ce9
fobossalmeron <fobos.salmeron@gmail.com>
2021-02-01 14:57:41 -0600

    with next offline

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//   enabled: process.env.ANALYZE === "true",
+// });
 const path = require("path");
 // const withPWA = require("next-pwa");
-// const withOffline = require("next-offline");
+const withOffline = require("next-offline");
 const caching = require("./utils/caching");
 
 const nextConfig = {
@@ -63,6 +63,6 @@ const nextConfig = {
   },
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+// module.exports = withBundleAnalyzer(nextConfig);
 // module.exports = withPWA(nextConfig);
-// module.exports = withOffline(nextConfig)
+module.exports = withOffline(nextConfig);
